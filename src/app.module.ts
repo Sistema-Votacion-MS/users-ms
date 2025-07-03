ab10dba60995f0e1b3f4b7e3b765ba458e2ffb35
Jose Luis Cusilayme Garcia <jcusilaymeg@unsa.edu.pe>
2025-07-03 01:04:49 -0500

    feat: add @nestjs/config package and configure global settings in AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { PrismaModule } from './prisma/prisma.module';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [UsersModule],
-  controllers: [],
-  providers: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    PrismaModule,
+    UsersModule,
+  ],
 })
 export class AppModule {}
